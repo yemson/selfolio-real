ad821ed1b0b9dc0606e3bee9b143e83c82cf7429
yemson <yemson@icloud.com>
2025-03-26 00:37:10 +0900

    feat: 스킬 뱃지 추가

---
+++ app/components/global/Badge.vue
@@ -0,0 +1,16 @@
+<template>
+  <span
+    class="inline-flex h-5 items-center gap-1 rounded bg-neutral-200/50 px-1.5 text-xs font-medium text-neutral-600"
+  >
+    {{ props.label }}
+  </span>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: [
-    "@nuxt/ui",
-    "@nuxt/eslint",
-    "@nuxt/content",
-    "@nuxtjs/supabase",
-    "@nuxtjs/mdc",
-  ],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/supabase", "@nuxtjs/mdc"],
 
   supabase: {
     redirect: false,
